81b02ce971eed45211ca7125d4592033660a6bcb
Link4Electronics <link4electronics@gmail.com>
2026-03-03 04:16:51 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export ICON=https://raw.githubusercontent.com/xyproto/zsnes/refs/heads/main/icon
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/zsnes
+quick-sharun ./AppDir/bin/zsnes /usr/lib32/libEGL.so*
 
 # Additional changes can be done in between here
 
