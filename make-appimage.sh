5930500567539cef7c667cb9b690409082200839
Link4Electronics <link4electronics@gmail.com>
2026-03-07 09:48:37 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,19 +5,19 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=https://raw.githubusercontent.com/xyproto/zsnes/refs/heads/main/icons/128x128x32.png
+export ICON=https://raw.githubusercontent.com/xyproto/zsnes/refs/heads/main/img/128x128x32.png
+export DESKTOP=https://raw.githubusercontent.com/xyproto/zsnes/refs/heads/main/linux/zsnes.desktop
 export DEPLOY_OPENGL=1
+export LIB_DIR=/usr/lib32
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/zsnes /usr/lib32/libEGL.so* /usr/lib32/libEGL_mesa.so*
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/zsnes
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
