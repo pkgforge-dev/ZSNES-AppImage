d77ae15ec827ab03c4b14cfa52af5b7795d73503
Link4Electronics <link4electronics@gmail.com>
2026-09-08 23:46:54 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,7 +19,7 @@ echo "Building ZSNES..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/xyproto/zsnes"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./zsnes
+git clone --depth 1 "$REPO" ./zsnes
 git -C ./zsnes apply "$PWD"/patches/fix-gui-wallpaper-click-crash.patch
 echo "$VERSION" > ~/version
 
